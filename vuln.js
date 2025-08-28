4e0b2c638437ee99db299f89fb5caab96b1c772a
Halvard Hagesæther <hhages@equinor.com>
2025-08-28 15:00:21 +0200

    Create vuln.js

---
+++ vuln.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+// CodeQL should flag this as unsafe deserialization / eval
+app.get('/eval', (req, res) => {
+  const input = req.query.code;
+  eval(input);  // ❌ dangerous, CodeQL will catch it
+  res.send('Executed');
+});
+
+app.listen(3000);
